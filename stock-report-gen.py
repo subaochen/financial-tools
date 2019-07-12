e60935bfff95599906b7f468d39e86bb63620dfe
subaochen <subaochen@126.com>
2019-07-12 15:41:11 +0800

    采纳zzliu的建议代码

--- stock-report-gen.py
+++ stock-report-gen.py
@@ -46,23 +46,34 @@ def analyse_report(code, rows, cols):
     data.insert(0, '股票代码', code)
     return data
 
+def each_record(codes, years, row='营业收入(万元)'):
+    for code in codes:
+        filename = f'data/lrb_{code}.csv'
+        df = pd.read_csv(filename, index_col=0)
+        x = df.loc[row, years]
+        yield [code] + list(x)
 
 if __name__ == '__main__':
     # 测试股票代码集合
     codes = ['603730', '603757']
-    summary_data = []
+    years = ['2018-12-31', '2017-12-31', '2016-12-31']
+    #summary_data = []
     for code in codes:
         if DOWNLOAD == True:
+            print(f"downloading report:{code}")
             download_report(code)
         # 第一列（2018年数据），第12行（研发费用）
-        data = analyse_report(code, [12], [1])
-        summary_data.append(data.values.tolist())
+        #data = analyse_report(code, [12], [1])
+        #summary_data.append(data.values.tolist())
 
-    with open("summary.csv", "w+") as csvfile:
-        writer = csv.writer(csvfile)
-        # 根据情况写入表头
-        writer.writerow(["股票代码", "研发费用(万元)"])
-        # 写入多行用writerows
-        for item in summary_data:
-            writer.writerows(item)
+    summary = pd.DataFrame.from_records(each_record(codes, years), columns=['code'] + years)
+    summary.to_csv('tmp.csv', index=False)
+
+    #with open("summary.csv", "w+") as csvfile:
+    #    writer = csv.writer(csvfile)
+    #    # 根据情况写入表头
+    #    writer.writerow(["股票代码", "研发费用(万元)"])
+    #    # 写入多行用writerows
+    #    for item in summary_data:
+    #        writer.writerows(item)
 
