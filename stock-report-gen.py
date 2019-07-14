baca3ea686f3d9c10afbdb71f12f229e5f40f4e7
subaochen <subaochen@126.com>
2019-07-14 17:00:58 +0800

    following zzliu's suggestion

--- stock-report-gen.py
+++ stock-report-gen.py
@@ -2,7 +2,6 @@
 
 import urllib3
 import pandas as pd
-import csv
 import os
 
 # if DOWNLOAD = False，don't download report
@@ -29,24 +28,8 @@ def download_report(code):
         f.write(response.data.decode('gbk').encode('utf-8'))
 
 
-def analyse_report(code, rows, cols):
-    """
-    分析报表
-    Arguments:
-        code:股票代码
-        rows:读取报表的哪些行，接受列表
-        cols:读取报表的哪些列，接受列表
-
-    Return:
-        获取的数据
-    """
-    filename = f'data/lrb_{code}.csv'
-    df = pd.read_csv(filename)
-    data = df.iloc[rows, cols]
-    data.insert(0, '股票代码', code)
-    return data
-
-def each_record(codes, years, row='营业收入(万元)'):
+# 默认获取研发费用，修改row可以获取其他资料
+def each_record(codes, years, row='研发费用(万元)'):
     for code in codes:
         filename = f'data/lrb_{code}.csv'
         df = pd.read_csv(filename, index_col=0)
@@ -56,24 +39,12 @@ def each_record(codes, years, row='营业收入(万元)'):
 if __name__ == '__main__':
     # 测试股票代码集合
     codes = ['603730', '603757']
-    years = ['2018-12-31', '2017-12-31', '2016-12-31']
-    #summary_data = []
+    years = ['2018-12-31', '2017-12-31']
     for code in codes:
         if DOWNLOAD == True:
             print(f"downloading report:{code}")
             download_report(code)
-        # 第一列（2018年数据），第12行（研发费用）
-        #data = analyse_report(code, [12], [1])
-        #summary_data.append(data.values.tolist())
 
     summary = pd.DataFrame.from_records(each_record(codes, years), columns=['code'] + years)
-    summary.to_csv('tmp.csv', index=False)
-
-    #with open("summary.csv", "w+") as csvfile:
-    #    writer = csv.writer(csvfile)
-    #    # 根据情况写入表头
-    #    writer.writerow(["股票代码", "研发费用(万元)"])
-    #    # 写入多行用writerows
-    #    for item in summary_data:
-    #        writer.writerows(item)
+    summary.to_csv('summary.csv', index=False)
 
